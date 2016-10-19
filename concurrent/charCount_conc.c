3d632dd06b7edd2f77b3d6c12b846a9b59edb917
Vinícius Garcia <vingarciasc@gmail.com>
2016-10-19 16:42:50 -0200

    v0.2.4 - Versão concorrente funciona com duas threads (ocorrem certos erros para mais de duas threads); - Alocação dinâmica da frequência; - Cada thread possui uma frequência local que é somada ao final do processamento.

--- concurrent/charCount_conc.c
+++ concurrent/charCount_conc.c
@@ -7,19 +7,23 @@
 #define VERDE 0
 #define AMARELO 1
 #define VERMELHO 2
+#define true 1
+#define false 0
 
-void charIncrement(char c);
+void charIncrement(char c, int id);
 void writeCount();
 void printBuffer();
 void* produtor(void* tid);
 void* consumidor(void* tid);
 
 FILE *input, *output;
-long long int freq[256] = {0};
-int bufferSize, bufferCount = 0;
-int fileEnded = 0, nthreads_cons;
+int bufferSize, bufferCount = 0, lastCharacter;
+int fileEnded = 0, nthreads_cons, threads_cons_left = nthreads_cons;
+int* isBlocoFull;
 pthread_cond_t cond_cons, cond_prod; pthread_mutex_t mutex;
 char* buffer;
+long long int** freq;
+long long int freq_total[256] = {0};
 
 int main(int argc, char *argv[]) {
 	// Initialize variables and data structures
@@ -47,9 +51,21 @@ int main(int argc, char *argv[]) {
 		exit(1);
 	}
 
+	nthreads_cons = atoi(argv[3]);
 	//Initialize character buffer
 	bufferSize = 10; //this should be a parameter
 	buffer = (char*) malloc(sizeof(char) * bufferSize);
+	isBlocoFull = (int*) malloc(sizeof(char) * nthreads_cons);
+	for (int i = 0; i < nthreads_cons; i++)
+		isBlocoFull[i] = false;
+
+	freq = (long long int**) malloc(sizeof(long long int*) * nthreads_cons);
+	for (int i = 0; i < nthreads_cons; i++) {
+		freq[i] = (long long int*) malloc(sizeof(long long int) * 256);
+		for (int c = 0; c < 256; c++) {
+			freq[i][c] = 0;
+		}
+	}
 
 	GET_TIME(fim);
 	printf(">> Tempo de inicialização: %.8lf\n", fim - inicio);
@@ -66,7 +82,6 @@ int main(int argc, char *argv[]) {
 	}
 
 	//Initialize threads consumidoras
-	nthreads_cons = atoi(argv[3]);
 	pthread_t* threads_cons = (pthread_t*) malloc(sizeof(pthread_t) * nthreads_cons);
 	if (threads_cons == NULL) {
 		printf("Error: could not allocate memory for threads consumidoras\n");
@@ -112,6 +127,7 @@ int main(int argc, char *argv[]) {
 	fclose(input);
 	fclose(output);
 	free(threads_cons);
+	free(freq);
 	pthread_cond_destroy(&cond_cons);
 	pthread_cond_destroy(&cond_prod);
 	pthread_mutex_destroy(&mutex);
@@ -125,7 +141,7 @@ int main(int argc, char *argv[]) {
 
 void* consumidor(void* tid) {
 	int id = * (int*) tid;
-	printf(">> Thread consumidora #%d iniciada\n", id);
+	printf(">> Thread consumidora #%d iniciada\n", id + 1);
 	int bloco = bufferSize / nthreads_cons;
 	int inicio = id * bloco;
 	int fim = inicio + bloco;
@@ -134,30 +150,32 @@ void* consumidor(void* tid) {
 	printf("inicio: %d, fim: %d, bloco: %d, thread: %d\n", inicio, fim, bloco, id + 1);
 
 	while (1) {
-		//printf("C1\n");
+		printf("T%d - C1\n", id + 1);
 		pthread_mutex_lock(&mutex);
-		while (bufferCount != bufferSize && fileEnded == 0) {
+		while (isBlocoFull[id] == false && fileEnded == 0) {
 			pthread_cond_wait(&cond_cons, &mutex);
 		}
 		pthread_mutex_unlock(&mutex);
-		//printf("C2\n");
+		printf("T%d - C2\n", id + 1);
 
-		for (int i = inicio; i < fim && i < bufferCount; i++) {
-			charIncrement(buffer[i]);
+		for (int i = inicio; i < fim && i < lastCharacter; i++) {
+			charIncrement(buffer[i], id);
 			buffer[i] = '_';
-			printBuffer();
+			//printf("T%d: ", id + 1); printBuffer();
 		}
 		
 		if (fileEnded == 1) break;
-		//printf("C3\n");
+		printf("T%d - C3\n", id + 1);
 		pthread_mutex_lock(&mutex);
+		isBlocoFull[id] = false;
 		bufferCount -= bloco;
+		if (bufferCount == 0) pthread_cond_signal(&cond_prod);
+		printf("bufferCount: %d\n", bufferCount);
 		pthread_mutex_unlock(&mutex);
-		pthread_cond_signal(&cond_prod);
-		//printf("C4\n");
+		printf("T%d - C4\n", id + 1);
 	}
 
-	printf(">> Thread consumidora #%d encerrada\n", id);
+	printf(">> Thread consumidora #%d encerrada\n", id + 1);
 	pthread_exit(NULL);
 }
 
@@ -165,32 +183,36 @@ void* produtor(void* tid) {
 	char c;
 	printf(">> Thread produtora iniciada\n");
 	while (1) {
-		//printf("P1\n");
+		printf("P1\n");
 		pthread_mutex_lock(&mutex);
 		while (bufferCount != 0) {
 			pthread_cond_wait(&cond_prod, &mutex);
 		}
 		pthread_mutex_unlock(&mutex);
-		//printf("P2\n");
+		printf("P2\n");
 
 		for (int i = 0; i < bufferSize; i++) {
 			c = getc(input);
 			if (c == EOF) {
 				printf(">> Input file has been read.\n");
 				fileEnded = 1;
-				bufferCount = i;
+				lastCharacter = i;
 				pthread_cond_broadcast(&cond_cons);
 				pthread_exit(NULL); }
 			buffer[i] = c;
 		}
 
-		//printf("P3\n");
-		printBuffer();
+		printf("P3\n");
+		//printBuffer();
 		pthread_mutex_lock(&mutex);
+		lastCharacter = bufferSize;
 		bufferCount = bufferSize;
+		for (int i = 0; i < nthreads_cons; i++)
+			isBlocoFull[i] = true;
+		printf("bufferCount: %d\n", bufferCount);
 		pthread_cond_broadcast(&cond_cons);
 		pthread_mutex_unlock(&mutex);
-		//printf("P4\n");
+		printf("P4\n");
 	}
 }
 
@@ -204,18 +226,28 @@ void printBuffer() {
 	printf("]\n");
 }
 
-void charIncrement(char c) {
+void charIncrement(char c, int id) {
 	if((c >= '?' && c <= 'Z') || (c >= 'a' && c <= 'z') || (c >= '0' && c <= ';') 
-		|| (c >= '#' && c <= '&') || c == '!' || c == '.' || c == '_' || c == '-' || c == '(' || c == ')')
-		freq[c]++;
+		|| (c >= '#' && c <= '&') || c == '!' || c == '.' || c == '_' || c == '-' || c == '(' || c == ')') {
+		//printf("freq[%d][%c] = %lld\n", id, c, freq[id][c]);
+		freq[id][c]++;
+	}
 }
 
 // Write the character frequencies to the output file
 void writeCount() {
 	int c; long long int count;
 
+	for (int i = 0; i < nthreads_cons; i++) {
+		printf("Thread #%d - Resultados:\n", i + 1);
+		for (int d = 0; d < 256; d++) {
+			if (freq[i][d] != 0) printf("%c, %lld\n", d, freq[i][d]);
+			freq_total[d] += freq[i][d];
+		}
+	}
+
 	fprintf(output, "Caractere, Qtde\n");
 	for (c = 0; c < 256; c++)
-		if ((count = freq[c]) != 0)
+		if ((count = freq_total[c]) != 0)
 			fprintf(output, "%c, %lld\n", c, count);
 }
